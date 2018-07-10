a7024777d60d6dbf7857ab82559ebea10510c449
yuli <yuli-ipatova@mail.ru>
2018-07-10 23:05:29 +0300

    add summa component

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ console.log(division(3, 5));
 console.log(differents(3, 5));
 console.log(multiplication(3, 5));
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './src/hello';
+import Summa from './src/summa';
+
+
+ReactDOM.render (
+    <Hello />,
+    document.getElementById('root')
+);
+
+ReactDOM.render (
+    <Summa />,
+    document.getElementById('summa')
+);
---
+++ src/summa.js
@@ -0,0 +1,12 @@
+import { summa } from './arifmetica';
+import React, { Component } from 'react';
+
+class Summa extends Component {
+    render() {
+        return (
+            <div>Summa: {summa(3, 5)}</div>
+        )
+    }
+}
+
+export default Summa;
